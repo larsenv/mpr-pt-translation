0d29582786bb7b5061023a075f9905d528cab84d
larsenv <larsen@Weborican.com>
2026-06-12 17:54:31 -0500

    Rebase onto original better guestz.py formatting and dictionary keys

--- tools/make_xml.py
+++ tools/make_xml.py
@@ -1,26 +1,35 @@
-import guestz
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 import binascii
 import re
+import ast
 
 def remove_invalid_xml_chars(val):
     if not isinstance(val, str):
         return val
-    # Remove control characters except for \t (0x09), \n (0x0A), \r (0x0D)
-    # \x00 is specifically not allowed in XML
     return re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f]', '', val)
+
+namespace = {}
+with open("guestz.py", "r", encoding="utf-8") as f:
+    source = f.read()
+    # prevent subprocess execution
+    source = re.sub(r'subprocess\.run\s*\(.*?\)', 'pass', source, flags=re.DOTALL)
+    exec(source, namespace)
+
+mii_list = namespace['mii']
+pokemon_list = namespace['pokemon']
+text_list = namespace['text']
 
 root = ET.Element("characters")
 
 locales = ["en_US", "ja_JP", "de_DE", "fr_FR", "es_ES", "it_IT"]
+all_keys = ["dialog_1", "exchange_1", "mail_1", "mail_2", "dialog_2", "dialog_3", "name_1", "name_2", "name_from", "extra", "pad"]
 
-for i in range(len(guestz.mii)):
-    mii_hex = guestz.mii[i]
-    pokemon_hex = guestz.pokemon[i] if i < len(guestz.pokemon) else ""
-    text_data = guestz.text[i] if i < len(guestz.text) else {}
+for i in range(len(mii_list)):
+    mii_hex = mii_list[i]
+    pokemon_hex = pokemon_list[i] if i < len(pokemon_list) else ""
+    text_data = text_list[i] if i < len(text_list) else {}
     
-    # decode name
     if mii_hex:
         mii_bytes = binascii.unhexlify(mii_hex)
         name_bytes = mii_bytes[2:18]
@@ -38,24 +47,29 @@ for i in range(len(guestz.mii)):
     
     text_node = ET.SubElement(char_node, "text")
     
-    # Figure out number of strings from existing locales
-    num_strings = 0
+    char_keys = set()
     for loc, strings in text_data.items():
-        if len(strings) > num_strings:
-            num_strings = len(strings)
+        if isinstance(strings, dict):
+            for k in strings.keys():
+                char_keys.add(k)
+    
+    ordered_keys = [k for k in all_keys if k in char_keys]
+    for k in char_keys:
+        if k not in ordered_keys:
+            ordered_keys.append(k)
             
-    if num_strings == 0:
-        num_strings = 9 # default
+    if not ordered_keys:
+        ordered_keys = all_keys # fallback
         
     for loc in locales:
         loc_node = ET.SubElement(text_node, loc)
         
-        strings = text_data.get(loc, [])
-        for j in range(num_strings):
-            str_node = ET.SubElement(loc_node, "string", index=str(j))
-            if j < len(strings) and strings[j]:
-                # replace literal null byte or newlines if we want, or just put them as is
-                str_node.text = remove_invalid_xml_chars(strings[j])
+        strings = text_data.get(loc, {})
+        for k in ordered_keys:
+            str_node = ET.SubElement(loc_node, "string", key=k)
+            val = strings.get(k, "")
+            if val:
+                str_node.text = remove_invalid_xml_chars(val)
             else:
                 str_node.text = ""
 
