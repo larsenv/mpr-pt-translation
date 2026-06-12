a8bae756cb7a318f53983c5623fe2bc52e2339e1
larsenv <larsen@Weborican.com>
2026-06-12 18:00:34 -0500

    Support events with up to 3 guests and normalize XML keys

--- tools/guestz.py
+++ tools/guestz.py
@@ -47,29 +47,46 @@ import xml.etree.ElementTree as ET
 tree = ET.parse("guestz.xml")
 root = tree.getroot()
 
+# Get the first event
+event_node = root.find("event")
+if event_node is None:
+    raise ValueError("No event found in guestz.xml")
+
+event_type_hex = event_node.attrib.get("action", "187")
+event_type = int(event_type_hex, 16)
+
 pokemon = []
 mii = []
 text = []
 
-for char_node in root.findall("character"):
-    mii.append(char_node.find("mii").text or "")
-    pokemon.append(char_node.find("pokemon").text or "")
-    
-    char_text = {}
-    text_node = char_node.find("text")
-    if text_node is not None:
-        for loc_node in text_node:
-            loc = loc_node.tag
-            char_text[loc] = {}
-            for str_node in loc_node.findall("string"):
-                key = str_node.attrib.get("key")
-                val = str_node.text or ""
-                char_text[loc][key] = val
-                
-    text.append(char_text)
+chars = list(event_node.findall("character"))
+
+# Pad up to 3 guests
+for i in range(3):
+    if i < len(chars):
+        char_node = chars[i]
+        mii.append(char_node.find("mii").text or "")
+        pokemon.append(char_node.find("pokemon").text or "")
+        
+        char_text = {}
+        text_node = char_node.find("text")
+        if text_node is not None:
+            for loc_node in text_node:
+                loc = loc_node.tag
+                char_text[loc] = {}
+                for str_node in loc_node.findall("string"):
+                    key = str_node.attrib.get("key")
+                    val = str_node.text or ""
+                    char_text[loc][key] = val
+        text.append(char_text)
+    else:
+        # Empty guest
+        mii.append("00" * 74)
+        pokemon.append("00" * 136)
+        text.append({})
 
 # Append the \0 terminator only to populated strings
-en_US_text_list = [val + "\0" if val else "" for val in text[3]["en_US"].values()]
+en_US_text_list = [val + "\0" if val else "" for val in text[0].get("en_US", {}).values()]
 
 
 def get_utf16_len(count):
@@ -79,10 +96,22 @@ def get_utf16_len(count):
 celebrity = {
     "unknown1": binascii.unhexlify("04ffff00ffff00"),
     "unknown2": binascii.unhexlify("00000000000000000000000000000000000789"),
-    "wanted_pokemon": u16(66),
-    "celebrity_mii": binascii.unhexlify(mii[1]),
-    "celebrity_mii_xmodem": u16(crc16_xmodem(binascii.unhexlify(mii[1]))),
-    "celebrity_pokemon": binascii.unhexlify(pokemon[3]),
+    
+    "wanted_pokemon_0": u16(66),
+    "celebrity_mii_0": binascii.unhexlify(mii[0]),
+    "celebrity_mii_xmodem_0": u16(crc16_xmodem(binascii.unhexlify(mii[0]))),
+    "celebrity_pokemon_0": binascii.unhexlify(pokemon[0]),
+
+    "wanted_pokemon_1": u16(66 if mii[1] != "00"*74 else 0),
+    "celebrity_mii_1": binascii.unhexlify(mii[1]),
+    "celebrity_mii_xmodem_1": u16(crc16_xmodem(binascii.unhexlify(mii[1]))),
+    "celebrity_pokemon_1": binascii.unhexlify(pokemon[1]),
+
+    "wanted_pokemon_2": u16(66 if mii[2] != "00"*74 else 0),
+    "celebrity_mii_2": binascii.unhexlify(mii[2]),
+    "celebrity_mii_xmodem_2": u16(crc16_xmodem(binascii.unhexlify(mii[2]))),
+    "celebrity_pokemon_2": binascii.unhexlify(pokemon[2]),
+
     "opening_timestamp_1": u32(opening_timestamp + (1209599 * 10)),
     "closing_timestamp_1": u32(opening_timestamp + (1209599 * 11)),
     "unknown_3": u16(0),
